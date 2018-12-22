7a4bd2bc32a5e99790d453c3f1b33e20f4162fc6
Ian Auld <imauld@gmail.com>
2018-12-22 14:58:52 -0800

    Add release script

---
+++ do_release_commit.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+PART=$1
+
+get_message () {
+    part=${1}
+    prevVer=$(waypoint latest kx)
+    newVer=$(waypoint bump kx --${part})
+    clog=$(tail -n +3 CHANGELOG.md)
+    clog=${clog%\#\#\# ${prevVer}*}
+    printf "%s\n\n${clog}" "Incrementing ${part} version from *${prevVer}* -> *${newVer}*"
+}
+
+do_commit() {
+   part=${1}
+   git commit --allow-empty -m "$(get_message ${part})"
+}
+
+do_tag() {
+	git tag $(waypoint latest kx)
+	git push --tags
+}
+
+do_push () {
+    git push origin master
+}
+
+do_commit ${PART}
+do_push
+do_tag
